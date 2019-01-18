42d672bb4351ce5bfa077efe711db8e766b96653
Dan Walker <daniel.walker@ncas.ac.uk>
2019-01-18 16:32:08 +0000

    Extend ncas-sonic-4 class, plus test data

---
+++ sonic-2d-3.py
@@ -0,0 +1,11 @@
+import importlib
+from ncas_sonic_4_software.sonic_2d import GillWindSonic
+
+class IAOSonic(GillWindSonic):
+    """
+    class to convert the Gill or Vaisala 2D Sonic datafile from the 
+    IAO to NetCDF
+    """
+
+    def __init__(self):
+        pass;
